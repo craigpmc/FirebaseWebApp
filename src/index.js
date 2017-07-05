de4948a72154fa78c99326da52d359f6cae650a9
Stefanescu Valentin <valentin.stefanescu@ynov.com>
2017-07-06 00:36:35 +0200

    Install firebase, react-rooter and clean project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './component/app/App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyA-oX24CIWpvrnT8VZsVLVJmkgIDE0BYDM",
+    authDomain: "moto-1ac54.firebaseapp.com",
+    databaseURL: "https://moto-1ac54.firebaseio.com",
+    projectId: "moto-1ac54",
+    storageBucket: "",
+    messagingSenderId: "558159409374",
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
