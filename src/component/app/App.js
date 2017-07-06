289b5d638058fb5fb81cb78b86dd43b61642dabd
Stefanescu Valentin <valentin.stefanescu@ynov.com>
2017-07-07 01:25:05 +0200

    Prepare fetch to api and average calc function

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -7,6 +7,26 @@ export default class App extends React.Component {
         document.title = "Home Page"
     }
 
+    onRecoverData = () => {
+        return fetch('https://ergast.com/api/f1/current/last/drivers.json', {
+            method: 'GET'
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.error('error', err);
+            throw err
+        })
+    };
+
+    // getPilotsAgesAverage = () => {
+    //     // map on array of DOB pilots
+    //     const date = new Date('1995/03/17');
+    //     const today = new Date();
+    //     const diff = Math.abs(today.getTime() - date.getTime());
+    //     const average = Math.ceil(diff / (1000 * 3600 * 24)) / 365;
+    //     return average / index
+    // };
+
   render() {
 
     return (
@@ -15,7 +35,9 @@ export default class App extends React.Component {
               <h2 className="mb-4 pb-2" style={{ borderBottom: '2px solid white' }}>Welcome !</h2>
               <img src={logo} width="150" height="150" className="mb-4"/>
               <p>Details: onclick button, display drivers ages of the last run.</p>
-              <button className="btn btn-secondary">See details</button>
+              <button className="btn btn-secondary" onClick={this.onRecoverData}>
+                  See details
+              </button>
           </div>
       </div>
     );
